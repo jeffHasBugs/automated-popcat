e9492c7a5c279fd8b67b82f4a23e3a315227b0d9
jeffhasbugs <jeffhasbugs@protonmail.com>
2021-08-17 18:51:06 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import random
+import time
+import urllib.request
+import urllib.parse
+
+# Didn't use requests 
+# https://stackoverflow.com/questions/62684468/pythons-requests-triggers-cloudflares-security-while-urllib-does-not
+
+# Recaptcha not needed for now
+# Getting the token requires sending a payload
+# along with the request that contains unique
+# information on your browser and computer.
+
+# def get_recaptcha_token(payload):
+#     url = "https://google.com/recaptcha/api2/reload?k=6Ledv1IaAAAAAKFJSR7VKPE8e-4kht4ZmLzencES"
+#     headers = { "Host": "recaptcha.net", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36", "Accept": "*/*", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate, br", "Content-Type": "application/x-protobuffer", "Content-Length": "8767", "Origin": "https://recaptcha.net", "DNT": "1", "Alt-Used": "recaptcha.net", "Connection": "keep-alive", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin" } 
+#     
+#     request_data = urllib.parse.urlencode(payload)
+#     request_data = request_data.encode("ascii")
+# 
+#     request = urllib.request.Request(url, data=request_data, headers=headers)
+#     r = urllib.request.urlopen(request).read()
+# 
+#     # cookie = r.headers["Set-Cookie"].split(";")[0]
+#     captcha_token = r.text.strip('][').split(',')[1].strip('"')
+#     
+#     return captcha_token
+
+
+def send_clicks(params):
+    url = "https://stats.popcat.click/pop"
+    headers = { "Host": "stats.popcat.click", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36", "Accept": "application/json, text/plain, */*", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate, br", "Origin": "https://popcat.click",  "Connection": "keep-alive", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-site", "Content-Type": "application/x-www-form-urlencoded" }
+
+    # encode additional data into bytes for body
+    request_data = urllib.parse.urlencode(params)
+    request_data = request_data.encode("ascii")
+
+    request = urllib.request.Request(url, data=request_data, headers=headers)
+    r = urllib.request.urlopen(request).read()
+    
+    return r.decode("utf-8")
+
+
+print("This is open source software. See LICENSE.")
+print("==================================================================")
+print("Sending clicks on a 30-second interval. Press CTRL + C to stop me.")
+print("==================================================================")
+
+while True:
+    params = dict()
+    params["pop_count"] = random.randint(750, 800)
+    params["captcha_token"] = "NULL"
+
+    print("Sending", params["pop_count"], "clicks")
+    response = send_clicks(params)
+    print(response)
+
+    time.sleep(30)
